c6340522879290103bfb4d557b3718e514dfbdbe
robinbutler <r.l.butler40@gmail.com>
2020-02-10 12:04:50 +0000

    added index.js files for future data routing simplicity

---
+++ db/data/index.js
@@ -0,0 +1,11 @@
+const ENV = process.env.NODE_ENV || "development";
+
+const testData = require("./test-data/index");
+const devData = require("./development-data/index");
+
+const data = {
+  test: testData,
+  development: devData
+};
+
+module.exports = data[ENV];
